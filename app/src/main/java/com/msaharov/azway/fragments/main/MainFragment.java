6684994c91eaecb1101d4766394b32a690e8a942
Max <msaharovforwork@gmail.com>
2023-12-04 20:27:19 +0300

    Навигация. Каркас.

--- app/src/main/java/com/msaharov/azway/fragments/main/MainFragment.java
+++ app/src/main/java/com/msaharov/azway/fragments/main/MainFragment.java
@@ -13,6 +13,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,58 +33,36 @@ public class MainFragment extends Fragment {
         return new MainFragment();
     }
 
-    private DrawerLayout drawerLayout;
-    public Toolbar toolbar;
-    private NavigationView navigationView;
 
-    public ImageView menuIV;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        binding = FragmentMainBinding.inflate(inflater, container, false);
+        binding = FragmentMainBinding.inflate(getLayoutInflater(), container, false);
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        init();
+
         NavHostFragment host = (NavHostFragment) getChildFragmentManager().findFragmentById(R.id.host_fragment);
         NavController navController = host.getNavController();
-        NavigationUI.setupWithNavController(navigationView, navController);
-
-
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-        // TODO: Use the ViewModel
-    }
-
-    private void init() {
-
-        drawerLayout = binding.drawerLayout.findViewById(R.id.drawerLayout);
-        navigationView = binding.navView.findViewById(R.id.navView);
-        menuIV = binding.menuIV.findViewById(R.id.menuIV);
-        toolbar = binding.toolbar.findViewById(R.id.toolbar);
-        initListeners();
-    }
-
-    private void initListeners(){
-        menuIV.setOnClickListener(v -> {
-            if (!drawerLayout.isDrawerOpen(GravityCompat.START)) {
+        NavigationUI.setupWithNavController(binding.navView, navController);
+        binding.menuIV.setOnClickListener(v -> {
+            Log.d("Listener", "listened");
+            if (!binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
                 openDrawerLayout();
             }
-            else {drawerLayout.closeDrawer(GravityCompat.START);}
+            else {binding.drawerLayout.closeDrawer(GravityCompat.START);}
         });
+
+
     }
 
     public void openDrawerLayout() {
-        drawerLayout.openDrawer(GravityCompat.START);
-        drawerLayout.open();
+        binding.drawerLayout.openDrawer(GravityCompat.START);
+        binding.drawerLayout.open();
     }
 
 }
